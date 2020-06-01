70cd87b3dcceaa922ed375cfa54410a958625eee
Cristian Cazan <cristian.cazan@qubiz.com>
2020-06-01 16:55:36 +0300

    Added post list and post detail components

--- WebClientApp/src/app/app-routing.module.ts
+++ WebClientApp/src/app/app-routing.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostListComponent } from './features/post/components/post-list/post-list.component';
+import { PostDetailComponent } from './features/post/components/post-detail/post-detail.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/posts',
+    pathMatch: 'full'
+  },
+  {
+    path: 'posts',
+    component: PostListComponent
+  },
+  {
+    path: 'post',
+    component: PostDetailComponent,
+    children:
+      [
+        {
+          path: ':id',
+          component: PostDetailComponent
+        }
+      ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- WebClientApp/src/app/app.module.ts
+++ WebClientApp/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule }    from '@angular/common/http';
+import { PostListComponent } from './features/post/components/post-list/post-list.component';
+import { PostDetailComponent } from './features/post/components/post-detail/post-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent,
+    PostDetailComponent
   ],
   imports: [
     BrowserModule,
